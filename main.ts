9b6a9136f7100d90f117bd661a94d81c24a465cf
lakyung <90668130+lakyung@users.noreply.github.com>
2021-09-17 23:34:22 +0900

    초음파센서를 이용해 타이머 정지시키기

--- main.ts
+++ main.ts
@@ -2,11 +2,14 @@ input.onButtonPressed(Button.A, function () {
     초 += 1
     basic.showNumber(초)
 })
-input.onButtonPressed(Button.AB, function () {
+function 타이머정지 () {
     basic.showIcon(IconNames.Yes)
     music.stopAllSounds()
     pins.digitalWritePin(DigitalPin.P0, 0)
     초 = 0
+}
+input.onButtonPressed(Button.AB, function () {
+    타이머정지()
 })
 input.onButtonPressed(Button.B, function () {
     countdown = 초
@@ -19,7 +22,18 @@ input.onButtonPressed(Button.B, function () {
     music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Forever)
     pins.digitalWritePin(DigitalPin.P0, 1)
 })
+let 거리 = 0
 let countdown = 0
 let 초 = 0
 초 = 0
 countdown = 0
+// 초음파센서 이용해서 타이머 정지시키기
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    거리 = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 40
+    if (거리 <= 50) {
+        타이머정지()
+    }
+})
