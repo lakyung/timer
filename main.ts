15305b5dd5e4dcd56dfb8a0dd77be6ea28e5ebb6
lakyung <90668130+lakyung@users.noreply.github.com>
2021-09-17 15:52:17 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    시간 += 1
-    basic.showNumber(시간)
+    분 += 1
+    basic.showNumber(분)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showIcon(IconNames.Yes)
+    music.stopAllSounds()
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 시간; index++) {
+    for (let index = 0; index < 분; index++) {
         basic.pause(60000)
+        분 = 분 - 1
+        basic.showNumber(분)
     }
+    basic.showIcon(IconNames.EigthNote)
+    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Forever)
+    pins.digitalWritePin(DigitalPin.P0, 1)
 })
-let 시간 = 0
-시간 = 0
+let 분 = 0
+분 = 0
